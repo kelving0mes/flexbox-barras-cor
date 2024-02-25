ca91664d23e92666e8dd981001fd6ab97be712a5
Kelvin <kelvingomes04@gmail.com>
2024-02-25 20:35:05 -0300

    flex display assignment based on late 90's TV colored screen

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ display: "flex", flexDirection: "column", flex: 1, padding:"3px", backgroundColor:"black" }}>
+      <View style={{ display: "flex", flexDirection: "row", flex: 7 }}>
+        <View style={{ backgroundColor: "white", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#f8fb0d", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#11f5ea", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#06fa35", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#f608f5", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#f8050a", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#3409f4", flex: 1 }}></View>
+      </View>
+      <View style={{ display: "flex", flexDirection: "row", flex: 1 }}>
+        <View style={{ backgroundColor: "#3409f4", flex: 1 }}></View>
+        <View style={{ backgroundColor: "black", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#f608f5", flex: 1 }}></View>
+        <View style={{ backgroundColor: "black", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#11f5ea", flex: 1 }}></View>
+        <View style={{ backgroundColor: "black", flex: 1 }}></View>
+        <View style={{ backgroundColor: "white", flex: 1 }}></View>
+      </View>
+      <View style={{ display: "flex", flexDirection: "row", flex: 2 }}>
+        <View style={{ backgroundColor: "#316768", flex: 1 }}></View>
+        <View style={{ backgroundColor: "white", flex: 1 }}></View>
+        <View style={{ backgroundColor: "#660098", flex: 1 }}></View>
+        <View style={{ backgroundColor: "black", flex: 2 }}></View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
